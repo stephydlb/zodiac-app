81ba27cd5d534c1fa4573762794bac63758ee924
Stephydlb <stephkalubiaka@gmail.com>
2024-12-21 13:17:39 +0100

    updates

---
+++ src/utils/NotificationManager.js
@@ -0,0 +1,6 @@
+const notificationTexts = {
+  permission: "Autoriser les notifications pour recevoir votre horoscope quotidien",
+  daily: "Votre horoscope du jour est disponible !",
+  newFeatures: "Nouvelles prédictions disponibles",
+  error: "Impossible de charger l'horoscope"
+};
